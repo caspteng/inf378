a238cb234ebdc6e5bbb7924f586e03063957823f
Adrien Marion <adrien.marion@epitech.eu>
2021-02-11 17:58:23 +0100

    Improving design for Login and Register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,40 @@
 @extends('layouts.auth')
 @section('content')
     <h1>{{ $errors }}</h1>
-        <form method="POST" action="{{ route('login') }}" class="ui form">
-         @csrf
-        <div class="field">
-            <label for="email">Email</label>
-            <input id="email" type="email" name="email" placeholder="Zboub mail">
+    <form method="POST" action="{{ route('login') }}" class="ui form">
+        @csrf
+        <div class="ui placeholder segment">
+            <div class="ui two column very relaxed grid">
+                <div class="column">
+                    <div class="ui form">
+                        <div class="field">
+                            <label for="email">Email</label>
+                            <div class="ui left icon input">
+                                <input id="email" type="email" placeholder="votre@email.com">
+                                <i class="user icon"></i>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <label for="password">Mot de passe</label>
+                            <div class="ui left icon input">
+                                <input id="password" type="password" placeholder="Mot de passe">
+                                <i class="lock icon"></i>
+                            </div>
+                        </div>
+                        <button class="ui blue submit button" type="submit">Login</button>
+                    </div>
+                </div>
+                <div class="middle aligned column">
+                    <a href="{{ route('register') }}" class="ui big button">
+                        <i class="signup icon"></i>
+                        Sign Up
+                    </a>
+                </div>
+            </div>
+            <div class="ui vertical divider">
+                Or
+            </div>
         </div>
-        <div class="field">
-            <label for="password">Password</label>
-            <input id="password" type="password" name="password" placeholder="Your Zboub Password">
-        </div>
-        <button class="ui button" type="submit">Submit</button>
     </form>
 
 @stop
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,32 +2,41 @@
 @section('content')
 
     <h1>{{ $errors }}</h1>
-    <form method="POST" action="{{ route('register') }}" class="ui form">
+    <div class="ui attached message">
+        <div class="header">
+            Welcome to our site!
+        </div>
+        <p>Fill out the form below to sign-up for a new account</p>
+    </div>
+    <form method="POST" action="{{ route('register') }}" class="ui form attached fluid segment">
         @csrf
-
-        <div class="field">
-            <label for="username">Nom</label>
-            <input id="username" type="text" name="username" placeholder="Prénom Nom" required>
-        </div>
-        <div class="field">
-            <label for="email">Email</label>
-            <input id="email" type="email" name="email" placeholder="joe@schmoe.com" required>
-        </div>
-        <div class="field">
-            <label for="birthday">Date de naissance</label>
-            <input id="birthday" type="date" name="birthday" required>
-        </div>
-        <div class="two fields">
-        <div class="field">
-            <label for="password">Password</label>
-            <input id="password" type="password" name="password" placeholder="Your Zboub Password" required>
-        </div>
-        <div class="field">
-            <label for="password_confirmation">Password</label>
-            <input id="password_confirmation" type="password" name="password_confirmation" required>
-        </div>
-        </div>
-        <button class="ui button" type="submit">Submit</button>
+            <div class="field">
+                <label for="username">Nom</label>
+                <input id="username" type="text" name="username" placeholder="Prénom Nom" required>
+            </div>
+            <div class="field">
+                <label for="email">Email</label>
+                <input id="email" type="email" name="email" placeholder="joe@schmoe.com" required>
+            </div>
+            <div class="field">
+                <label for="birthday">Date de naissance</label>
+                <input id="birthday" type="date" name="birthday" required>
+            </div>
+            <div class="two fields">
+                <div class="field">
+                    <label for="password">Password</label>
+                    <input id="password" type="password" name="password" placeholder="Your Zboub Password" required>
+                </div>
+                <div class="field">
+                    <label for="password_confirmation">Password</label>
+                    <input id="password_confirmation" type="password" name="password_confirmation" required>
+                </div>
+            </div>
+            <button class="ui button" type="submit">Submit</button>
     </form>
+    <div class="ui bottom attached warning message">
+        <i class="icon help"></i>
+        Already signed up? <a href="#">Login here</a> instead.
+    </div>
 
 @stop
