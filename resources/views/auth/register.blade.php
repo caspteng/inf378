3f22f4ea0a8e662b5d2d6a947d831cb146ee2636
Adrien MARION <adrien.marion@epitech.eu>
2021-02-25 12:26:12 +0100

    Fixing an error in the birthdate field

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -21,7 +21,7 @@
             <label for="birthday">Date de naissance</label>
                 <div class="ui input left icon">
                     <i class="calendar icon"></i>
-                    <input id="birthday" name="birthday" type="date" placeholder="YY-mm-dd" {{ old('birthday') }}">
+                    <input id="birthday" name="birthday" type="date" placeholder="YY-mm-dd" value="{{ old('birthday') }}">
                 </div>
         </div>
         <div class="two fields">
