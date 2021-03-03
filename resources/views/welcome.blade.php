9d5fa6fb7c1c1ab20b271b4d06df85165f2b3553
Adrien MARION <adrien.marion@epitech.eu>
2021-03-04 00:03:20 +0100

    Improving homepage for small screen size

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,42 +21,40 @@
 
 
 <body>
+<div class="maincontainer">
+    <div class="wrapper">
+        <div class="ui equal height center aligned stackable grid">
 
-<div class="ui equal width center aligned middle aligned grid">
-    <div class="row ">
-        <div class="blue column tweet">
-
-            <div class="item ">
-                <span class="label"><i class="large search icon"></i> Retrouvez vos centres d'intérêt.</span>
-            </div>
-            <br>
-            <div class="item">
+            <div class="ui eight wide blue column tweet">
+                <div class="left-content">
+                    <div class="item">
+                        <span class="label"><i class="large search icon"></i> Retrouvez vos centres d'intérêt.</span>
+                    </div>
+                    <br>
+                    <div class="item">
                     <span class="label"><i class="large newspaper icon"></i> Discutez des tendances qui
                         <br>vous tiennent à cœur.</span>
+                    </div>
+                    <br>
+                    <div class="item">
+                        <span class="label"><i class="large heart icon"></i> Partagez, aimez, retweetez...</span>
+                    </div>
+                </div>
             </div>
-            <br>
-            <div class="item">
-                <span class="label"><i class="large heart icon"></i> Partagez, aimez, retweetez...</span>
-            </div>
-
-        </div>
-        <div class="column">
-            <div class="right-content middle aligned">
-            <h1><i class="twitter blue icon"></i> Ça se passe maintenant.</h1>
-            <span class="medium text">Rejoignez Tweet Académie.</span><br>
-            <a href="{{ route('login') }}" class="ui primary button">Se connecter
-                <i class="angle right icon"></i></a>
-            <a href="{{ route('register') }}" class="ui button">S'inscrire
-                <i class="angle right icon"></i></a>
+            <div class="ui eight wide column">
+                <div class="right-content">
+                    <h1><i class="twitter blue icon"></i> Ça se passe maintenant.</h1>
+                    <span class="medium text">Rejoignez Tweet Académie.</span><br>
+                    <a href="{{ route('login') }}" class="ui primary button">Se connecter
+                        <i class="angle right icon"></i></a>
+                    <a href="{{ route('register') }}" class="ui button">S'inscrire
+                        <i class="angle right icon"></i></a>
+                </div>
             </div>
         </div>
-    </div>
-
-    <div class="row" style="background-color: #e9e9e9;">
-        <div class="middle aligned column">
-            © 2021 Web@cadémie - TweetAcadémie
+        <div class="ui vertical footer segment center aligned" style="background-color: #e9e9e9;">
+            <p>© 2021 Web@cadémie - TweetAcadémie</p>
         </div>
-
     </div>
 </div>
 </body>
