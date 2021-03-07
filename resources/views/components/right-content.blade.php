9e653885b78e01399c266a85e5119302227f3863
Ellis-98 <pierre.schaefer@epitech.eu>
2021-03-07 15:46:08 +0100

    Editing nav-bar for mobile and desktop view

--- resources/views/components/right-content.blade.php
+++ resources/views/components/right-content.blade.php
@@ -1,9 +1,17 @@
 <div class="three wide column">
-    <div class="ui scrolling local search">
-        <div class="ui icon input">
-            <input class="prompt" type="text" placeholder="Recherche...">
-            <i class="search icon"></i>
-        </div>
-        <div class="results"></div>
+    <div class="ui blue segment">
+        <div class="ui horizontal left aligned divider header"><h3>Following</h3></div>
+            <p><img class="ui avatar image" src="assets/images/avatar/tom.jpg"><span>Username</span></p>
+            <p><img class="ui avatar image" src="assets/images/avatar/tom.jpg"><span>Username</span></p>
+            <p><img class="ui avatar image" src="assets/images/avatar/tom.jpg"><span>Username</span></p>
+            <p><img class="ui avatar image" src="assets/images/avatar/tom.jpg"><span>Username</span></p>
+    </div>
+    <hr>
+    <div class="ui blue segment">
+        <div class="ui horizontal left aligned divider header"><h3>Followers</h3></div>
+            <p><img class="ui avatar image" src="assets/images/avatar/tom.jpg"><span>Username</span></p>
+            <p><img class="ui avatar image" src="assets/images/avatar/tom.jpg"><span>Username</span></p>
+            <p><img class="ui avatar image" src="assets/images/avatar/tom.jpg"><span>Username</span></p>
+            <p><img class="ui avatar image" src="assets/images/avatar/tom.jpg"><span>Username</span></p>
     </div>
 </div>
