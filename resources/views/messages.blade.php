1ddc33e76713404137a61cb36ac1bfb04f35ded1
fallouache <faycal.allouache@epitech.eu>
2021-03-07 15:01:36 +0100

    Adding scrolling bar messages

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -7,25 +7,24 @@
         </div>
         <p>{{ TweetUser::getUsername($user->id) }}</p>
     </div>
-
     <form class="ui form  attached clearing segment" method="POST" action="{{ route('messages', $user->id) }}">
         @csrf
-
-        @foreach ($messages as $message)
-            <div class="ui field" style="@if ($message->sender->id == auth()->user()->id)
-                text-align: right;@endif">
-                <p><b><span>{{ $message->sender->surname }} </span></b>
-                    <span style="color: grey">le {{ $message->created_at->format('d/m/Y H:i') }} : </span>
-                </p>
-                <p>{{ $message->message }}</p>
-            </div>
-            <br>
-        @endforeach
+        <div class="" style="overflow-y: scroll; height: 500px">
+            @foreach ($messages as $message)
+                <div class="ui field" style="@if ($message->sender->id == auth()->user()->id)
+                    text-align: right;@endif">
+                    <p><b><span>{{ $message->sender->surname }} </span></b>
+                        <span style="color: grey">le {{ $message->created_at->format('d/m/Y H:i') }} : </span>
+                    </p>
+                    <p>{{ $message->message }}</p>
+                </div>
+                <br>
+            @endforeach
+        </div>
         <div class="field">
             <label for="message"></label>
             <textarea rows="2" id="message" placeholder="Écrivez votre message..." name="message"></textarea>
         </div>
         <button class="ui primary button">Envoyer</button>
     </form>
-
 @stop
